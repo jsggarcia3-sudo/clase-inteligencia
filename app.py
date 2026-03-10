a93e0cbfb1133b969687b3ff209db65c258bb6fa
jsggarcia3-sudo <jsggarcia3@gmail.com>
2026-03-10 14:45:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ else:
         st.info("Bienvenido. Acceda a los Módulos para estudiar el material completo.")
 
     elif seccion == "📚 Módulos":
-        modulo_selec = st.selectbox("Seleccione Módulo de Estudio:", ["Módulo 1: Conceptualización", "Módulo 2: Ciclo de Inteligencia", "Módulo 3: Recolección", "Módulo 4: Tratamiento"])
+        modulo_selec = st.selectbox("Seleccione Módulo de Estudio:", ["Módulo 1: Conceptualización", "Módulo 2: Ciclo de Inteligencia", "Módulo 3: Recolección", "Módulo 4: Tratamiento", "Módulo 5: Análisis"])
         
         # --- MÓDULO 1 ---
         if modulo_selec == "Módulo 1: Conceptualización":
