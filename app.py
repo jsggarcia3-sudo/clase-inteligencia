86706fe768f7d89fb1ee52908422555921b82f52
jsggarcia3-sudo <jsggarcia3@gmail.com>
2026-03-11 16:49:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -985,9 +985,9 @@ else:
                 if st.button("🚀 INICIAR EXAMEN MÓDULO 6"):
                     st.session_state['modo_examen'] = True
                     st.rerun()
-               else:
-                    st.markdown("<h2 style='text-align: center; color: #D4AF37;'>📝 EVALUACIÓN: COMUNICACIÓN E INTEGRACIÓN</h2>", unsafe_allow_html=True)
-                    st.warning("Asegúrese de haber comprendido los protocolos de seguridad antes de enviar sus respuestas.")
+        else:
+                st.markdown("<h2 style='text-align: center; color: #D4AF37;'>📝 EVALUACIÓN: COMUNICACIÓN E INTEGRACIÓN</h2>", unsafe_allow_html=True)
+                st.warning("Asegúrese de haber comprendido los protocolos de seguridad antes de enviar sus respuestas.")
 
                 with st.form("examen_m6"):
                     # Pregunta 1: El propósito de la comunicación
@@ -1078,8 +1078,8 @@ else:
 
                     except Exception as e:
                         st.error(f"Falla de conexión al registrar nota: {e}")
- 
-                if st.button("⬅️ Volver al Material"):
+
+                if st.button("⬅️ Salir del Examen"):
                     st.session_state['modo_examen'] = False
                     st.rerun()
 
